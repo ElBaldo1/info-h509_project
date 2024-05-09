7bb2d664bf3839cb051ab4d566d6318d9bbdea17
Ccroon17 <charles.croon@student-cs.fr>
2024-05-09 14:08:28 +0200

    update

--- WebPage.py
+++ WebPage.py
@@ -10,7 +10,7 @@ def accueil():
 
 @app.route('/search', methods=['GET'])
 def search():
-    query = flask.request.args.get('query')  # Récupère la requête de l'utilisateur depuis l'URL
+    query = flask.request.args.get('query')
     liste_départs=[]
     for element in ft.liveBoard(query)['departures']['departure']:
         timestamp = int(element['time'])
@@ -18,6 +18,25 @@ def search():
         heure = date.strftime('%H:%M')
         liste_départs.append(element['station']+"  "+ heure)
     return flask.render_template('gare_details.html', gare_name=query,liste_départs=liste_départs)
+
+@app.route('/depart', methods=['GET'])
+def depart():
+    #getting the full name of the ride
+    query=flask.request.args.get('destination')
+    #getting the destination
+    nom=query[0:-7]
+    #getting the hour
+    heure=query[-5:]+':00'
+    #transforming it into timestamp
+    heurebis=datetime.time(int(heure[0]+heure[1]),int(heure[3]+heure[4]),0)
+    #comparing the hours to know if the travel is today or tomorrow before getting the timestamp
+    if datetime.datetime.now().time()>heurebis:
+        heure_complete = datetime.datetime.combine(datetime.datetime.now().date() + datetime.timedelta(days=1), datetime.datetime.strptime(heure, '%H:%M:%S').time())
+        timestamp=heure_complete.timestamp()
+    else:
+        heure_complete = datetime.datetime.combine(datetime.datetime.now().date(), datetime.datetime.strptime(heure, '%H:%M:%S').time())
+        timestamp=heure_complete.timestamp()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
